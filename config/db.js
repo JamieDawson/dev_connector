3b0b370cc6fe96816add1fc8610ef6c5306851b3
Jamie Dawson <jadawson@e1z3r7p13.42.us.org>
2019-07-12 13:31:59 -0700

    finished video 13. Starting 14

--- config/db.js
+++ config/db.js
@@ -1,18 +1,21 @@
-//where the mongodb connection.
+
 const mongoose = require('mongoose');
-const config = require('config'); //used for mongoURI in default.json
+const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useCreateIndex: true
-    }); // returns a promise
-    console.log('MongoDB Connect...');
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
+    console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    process.exit(1); //exit process with failure
+    // Exit process with failure
+    process.exit(1);
   }
 };
 
---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config'); //for secret
+
+module.exports = function(req, res, next) {
+  //Get token from header
+  const token = req.header('x-auth-token'); //header key
+
+  //Check if no token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  //Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret')); //decode the token
+
+    req.user = decoded.user; //has user in payload.
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
