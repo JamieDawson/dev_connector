707ff5c07a04042db5ab3be8b734a5acfcaf22a9
Jamie Dawson <jadawson@e1z3r7p13.42.us.org>
2019-07-12 18:33:25 -0700

    finished video 16 and everything works. Going to start video 17 soon

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route           GET api/profile
-// @description     test route
-// @access          Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const Users = require('../../models/User');
+
+// @route           GET api/profile/me
+// @description     Get current users profile
+// @access          Private (beause we are sending in token)
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user',
+         ['name', 'avatar']);
+
+         if (!profile) {
+             return res.status(400).json({ msg: 'There is NO profile for this user'});
+         }
+
+         res.json(profile);
+    }catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server ERror');
+    }
+});
 
 module.exports = router;
