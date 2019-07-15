272371f4e4381654b0f70c92aef6a3db6f00b70a
Jamie Dawson <jadawson@e1z3r6p6.42.us.org>
2019-07-14 17:47:50 -0700

    Finished video 22. Starting video 23

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -250,4 +250,88 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
   }
 });
 
+// @route           PUT api/profile/education
+// @description     Add profile education
+// @access          Private
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'school is require')
+        .not()
+        .isEmpty(),
+      check('degree', 'Degree is require')
+        .not()
+        .isEmpty(),
+      check('fieldofstudy', 'Field of study is require')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is require')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    //Creates object with data that user submits.
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('SeRVer ERROR');
+    }
+  }
+);
+
+// @route           DELETE api/profile/education/:edu_id
+// @description     Delete educaiton from profile
+// @access          Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //Get remove index
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1); //splice out the index.
+
+    await profile.save(); //save profile
+
+    res.json(profile); //send profile back.
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('SeRVer ERROR');
+  }
+});
+
 module.exports = router;
