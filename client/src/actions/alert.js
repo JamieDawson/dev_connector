bd12eabe097a6c47d35c6b53082b5ee6ee6a61cd
Jamie Dawson <jadawson@e1z1r5p24.42.us.org>
2019-08-29 21:37:16 -0700

    finished video 39. Stil getting mystery error in iterm, but page loads just fine

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,10 +1,12 @@
 import uuid from 'uuid'; //IS THIS ACTUALLY IN MY CODE?!?!?!?!
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   const id = uuid.v4(); //gives random long string.
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout); // 5 seconds
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
+  //setAlert is deconstructed and pulled out of props
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,7 +21,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Password do not match');
+      setAlert('Password do not match', 'danger'); //check App.css for that css
     } else {
       console.log('SUCCESS');
     }
@@ -83,4 +87,11 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { setAlert }
+)(Register);
