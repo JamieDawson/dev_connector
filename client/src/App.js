bc12b0d536ad216c8c18184a4d6b27becf4fae20
Jamie Dawson <jadawson@e1z3r2p11.42.us.org>
2019-10-08 20:39:50 -0700

    finished video 60. Get ready for 61

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
-
 // Redux
 import { Provider } from 'react-redux'; //combines react and redux together
 import store from './store';
@@ -67,6 +67,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
